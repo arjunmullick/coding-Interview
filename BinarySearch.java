0a71f8085e0c58f6b1f030aff98c2736761c2473
arjunmullick <arjunmullick@gmail.com>
2020-12-16 20:03:41 -0500

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,41 @@
+package com.leetcode;
+
+public class BinarySearch {
+    //https://leetcode.com/problems/binary-search/
+
+    class Solution {
+        public int search(int[] nums, int target) {
+
+            int n = nums.length;
+
+            int l = 0;
+            int r = n - 1;
+
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (nums[mid] < target) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            if (r != n - 1 && nums[r + 1] == target) return r + 1;
+            else return -1;
+
+        }
+    }
+}
+
+/**
+    while(l<=r){
+        int mid = l + (r-l)/2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        if(nums[mid] < target){
+            l = mid+1;
+        }else{
+            r = mid -1;
+        }
+    }
+ **/
