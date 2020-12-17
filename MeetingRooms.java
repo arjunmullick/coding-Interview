e2f550a32ff6a028e3fe28fb1c38e9e67ba85fcc
arjunmullick <arjunmullick@gmail.com>
2020-12-16 19:59:47 -0500

    Add files via upload

---
+++ MeetingRooms.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+import java.util.PriorityQueue;
+
+public class MeetingRooms {
+
+    class Solution {
+        public boolean canAttendMeetings(int[][] intervals) {
+
+            PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b)->(a[0] - b[0]));
+
+            for(int[] i : intervals){
+                minHeap.offer(i);
+            }
+            int[] prev = new int[]{-1,-1};
+            while(minHeap.size() >0){
+                int next[] = minHeap.poll();
+                //as sorted due to heap prev[0] <= next[0] always
+                if(next[0] < prev[1] ) return false;
+                prev = next;
+            }
+
+            return true;
+        }
+    }
+
+/**
+
+ |---------|
+     |---------|
+
+ |-------|
+ |-----------|
+
+ **/
+}
