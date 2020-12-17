f0aa0347931bae16204456d61651a427128bd069
arjunmullick <arjunmullick@gmail.com>
2020-12-16 19:37:45 -0500

    Add files via upload

---
+++ LinkedListCycle.java
@@ -0,0 +1,35 @@
+package com.leetcode;
+
+public class LinkedListCycle {
+
+    //https://leetcode.com/problems/linked-list-cycle/
+
+    // * Definition for singly-linked list.
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            if (head == null || head.next == null) {
+                return false;
+            }
+            ListNode fast = head.next;// remember this is important to not start at same location. As first condition would terminate 
+            ListNode slow = head;
+            while(fast!=slow){
+                if(fast == null || fast.next == null){
+                    return false; // no loop
+                }
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            return true;
+        }
+    }
+
+
+}
