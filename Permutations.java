8c9e014f047c0b99aa57d3689f0411c319f0fa85
arjunmullick <arjunmullick@gmail.com>
2020-12-19 15:06:38 -0500

    Add files via upload

---
+++ Permutations.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    //https://leetcode.com/problems/permutations/
+    class Solution {
+        public List<List<Integer>> permute(int[] nums) {
+
+            List<List<Integer>> result = new ArrayList<>();
+            List<Integer> list = new ArrayList<>();//first list
+            for(int n : nums){
+                list.add(n);
+            }
+            backtrack(0,nums,list,result);
+            return result;
+        }
+
+        public void backtrack(int index, int[] nums, List<Integer> list,  List<List<Integer>> result){
+            if(index == list.size())//the recursion start swaping from last element
+                result.add(new ArrayList<>(list));//copy and add
+            for(int i = index ; i < nums.length ; i++){
+                swap(list,index,i);//swap all other with this index
+                backtrack(index+1,nums,list,result);
+                swap(list,index,i);//reset
+            }
+        }
+
+        private void swap(List<Integer> list , int index1 , int index2){
+            int temp = list.get(index1);
+            list.set(index1,list.get(index2));
+            list.set(index2,temp);
+        }
+    }
+}
