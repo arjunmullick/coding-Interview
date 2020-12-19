c28f8af8624bc4c9534df025d9c1a3662910b753
arjunmullick <arjunmullick@gmail.com>
2020-12-19 17:26:22 -0500

    Add files via upload

---
+++ Combinations.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations {
+    class Solution {
+        public List<List<Integer>> combine(int n, int k) {
+            List<List<Integer>> result = new ArrayList<>();
+            backtrack(1,n,k,new ArrayList<>(),result);
+            return result;
+        }
+
+        public void backtrack(int start, int n , int k, List<Integer> list, List<List<Integer>> result){
+
+            if(list.size() == k){
+                result.add(new ArrayList<>(list));
+
+            }
+
+            for(int i= start ; i <=n ; i++){
+                list.add(i);
+                backtrack(i+1,n,k,list,result);
+                list.remove(list.size()-1);
+            }
+        }
+    }
+}
