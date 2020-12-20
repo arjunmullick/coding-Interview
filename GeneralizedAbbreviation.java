280fca0de227b00c6c8da0d7b7dc02983b9da7a9
arjunmullick <arjunmullick@gmail.com>
2020-12-19 21:45:37 -0500

    Add files via upload

--- GeneralizedAbbreviation.java
+++ GeneralizedAbbreviation.java
@@ -7,7 +7,7 @@ public class GeneralizedAbbreviation {
 
     //Recursion
     class Solution {
-
+        //https://leetcode.com/problems/generalized-abbreviation/
         //word - 11rd or 1111 is not valid
         // ["4","3d","2r1","2rd","1o2","1o1d","1or1","1ord","w3","w2d","w1r1","w1rd","wo2","wo1d","wor1","word"]
         public List<String> generateAbbreviations(String word) {
