7be48652a9b8bc6e0411657a6ff665282b581622
arjunmullick <arjunmullick@gmail.com>
2020-12-20 14:34:56 -0500

    Add files via upload

---
+++ MaximumProductSubarray.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+public class MaximumProductSubarray {
+
+    //https://leetcode.com/problems/maximum-product-subarray/
+    class Solution {
+
+    /*
+    Many sub problem to simply took the pattern below.
+    // in once pass look for a max product of continious array from left to right
+    // in second pass look for a max product of continious array from right to left
+    // select max and return.
+    // Why it words ?
+    //  it covers all continious array growing in either direction example input = [-2 1 3]->  [-2,-2,-6] and [3,3,-6] at each index we select max
+    */
+
+        int max;
+
+        public int maxProduct(int[] nums) {
+            max = Integer.MIN_VALUE;
+            int product = 1;
+            for(int i = 0 ; i < nums.length ; i++){
+                product = product * nums[i];
+                max = Math.max(max,product);
+                if(product == 0) product =1; //reset for next continious array
+            }
+
+            product = 1;//reset for right to left search
+            for(int i = nums.length-1 ; i>=0 ; i--){
+                product = product * nums[i];
+                max = Math.max(max,product);
+                if(product == 0) product =1;//reset for next continious array
+            }
+
+            return max;
+        }
+    }
+}
