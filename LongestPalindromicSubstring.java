5440571d3935ceaba5bedb565e70bf4a73485d1f
arjunmullick <arjunmullick@gmail.com>
2020-12-20 16:37:51 -0500

    Add files via upload

---
+++ LongestPalindromicSubstring.java
@@ -0,0 +1,35 @@
+package com.leetcode;
+
+public class LongestPalindromicSubstring {
+    //https://leetcode.com/problems/longest-palindromic-substring/
+    //Expand at each location O(n^2)
+    class Solution {
+        int l;
+        int r;
+        int max = 0;
+        public String longestPalindrome(String s) {
+            if(s == null || s.length() == 0) return "";
+
+            if(s.length() == 1) return s;
+
+            for(int i = 0 ; i < s.length() ; i++){
+                findLongestPalindromicSubstring(s,i,i+1);
+                findLongestPalindromicSubstring(s,i,i);
+            }
+            return s.substring(l,r);
+        }
+
+        public void findLongestPalindromicSubstring(String s, int left, int right ){
+            while(left>=0 && right< s.length() &&  s.charAt(left) == s.charAt(right)){
+                left--;
+                right++;
+            }
+            int len = right - left - 1;
+            if(len > max){
+                max = len;
+                this.l = left+1;
+                this.r = right;
+            }
+        }
+    }
+}
