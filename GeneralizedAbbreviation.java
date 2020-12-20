c5c085a7be9386234318dc1c4c5c1c6d34391ca0
arjunmullick <arjunmullick@gmail.com>
2020-12-19 21:44:38 -0500

    Add files via upload

---
+++ GeneralizedAbbreviation.java
@@ -0,0 +1,35 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneralizedAbbreviation {
+
+    //Recursion
+    class Solution {
+
+        //word - 11rd or 1111 is not valid
+        // ["4","3d","2r1","2rd","1o2","1o1d","1or1","1ord","w3","w2d","w1r1","w1rd","wo2","wo1d","wor1","word"]
+        public List<String> generateAbbreviations(String word) {
+            List<String> result = new ArrayList<>();
+            backtrack(result,word,0,"",0);
+            return result;
+        }
+
+        private void backtrack(List<String> result, String word, int index, String current, int count){
+            if(index==word.length()){
+                if(count > 0) current += count;
+                result.add(current);
+            }
+            else{
+                backtrack(result, word, index+1, current, count + 1);
+                if(count > 0){
+                    current = current + count + word.charAt(index);
+                }else{
+                    current = current + word.charAt(index);
+                }
+                backtrack(result, word, index+1, current, 0);
+            }
+        }
+    }
+}
