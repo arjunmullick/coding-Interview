b74a8f260661cf0a685738492d42d46177ecaa5a
arjunmullick <arjunmullick@gmail.com>
2020-12-23 00:33:00 -0500

    Add files via upload

--- GraphValidTree.java
+++ GraphValidTree.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 public class GraphValidTree {
 
@@ -34,6 +35,51 @@ public class GraphValidTree {
                 for(int next : childs){
                     if(!visited[next]){
                         queue.offer(next);
+                        parent.put(next,node);
+                    }else if(parent.get(node) != next){//check the node parent is market as child then then its ok 0-1 , 1-0 because of undirected
+                        return false;//cyclic
+                    }
+                }
+
+            }
+            // All visited
+            for(boolean e : visited){
+                if(!e) return false;
+            }
+
+            return true;
+
+        }
+    }
+
+
+    class SolutionDFS {
+
+        //DFS
+        public boolean validTree(int n, int[][] edges) {
+            //in BFS if we visit all node and no cyclic in graph its a tree.
+            //check all nodes are part of tree
+            HashMap<Integer, List<Integer>> map = new HashMap<>();
+            HashMap<Integer,Integer> parent = new HashMap<>();
+            for(int[] edge : edges){
+                List<Integer> l1 = map.getOrDefault(edge[0],new ArrayList<>());
+                l1.add(edge[1]);
+                map.put(edge[0],l1);
+                List<Integer> l2 = map.getOrDefault(edge[1],new ArrayList<>());
+                l2.add(edge[0]);
+                map.put(edge[1],l2);
+            }
+
+            Stack<Integer> stack = new Stack<>();
+            boolean [] visited = new boolean[n];
+            stack.push(0);
+            while(stack.size() > 0){
+                int node = stack.pop();
+                visited[node] = true;
+                List<Integer> childs = map.getOrDefault(node, new ArrayList<>());//to not give null pointer
+                for(int next : childs){
+                    if(!visited[next]){
+                        stack.push(next);
                         parent.put(next,node);
                     }else if(parent.get(node) != next){//check the node parent is market as child then then its ok 0-1 , 1-0 because of undirected
                         return false;//cyclic
