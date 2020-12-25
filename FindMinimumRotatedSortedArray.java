d26a01226e48917a96fd5cf2c6ea452ee6e4e17d
arjunmullick <arjunmullick@gmail.com>
2020-12-25 16:42:57 -0500

    Add files via upload

---
+++ FindMinimumRotatedSortedArray.java
@@ -0,0 +1,20 @@
+package com.leetcode;
+
+public class FindMinimumRotatedSortedArray {
+
+    class Solution {
+
+        public int findMin(int[] nums) {
+            int l = 0, r = nums.length-1;
+            while (l <= r) {
+                int mid = l + (r-l)/2;
+                if (nums[mid] < nums[r]) {
+                    r = mid;
+                } else {
+                    l = mid+1;
+                }
+            }
+            return nums[l-1];
+        }
+    }
+}
