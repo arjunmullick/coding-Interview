a63d451573d3040866e0cbbbf37e7b657cd4dce3
arjunmullick <arjunmullick@gmail.com>
2020-12-25 17:39:12 -0500

    Add files via upload

--- PeakIndexMountainArray.java
+++ PeakIndexMountainArray.java
@@ -1,9 +1,32 @@
-package com.leetcode;
+package interview;
 
 public class PeakIndexMountainArray {
 
     //https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
     class Solution {
+        public int peakIndexInMountainArray(int[] arr) {
+
+            int n = arr.length;
+            int l = 0;
+            int r = n-1;
+            while(l<r){
+                int mid = l + (r-l)/2;
+
+                if(arr[mid] < arr[mid+1]){
+                    //number are increasin so peak is on right
+                    l = mid +1;
+                }else{
+                    //mid or number before it is peak
+                    r = mid;
+                }
+            }
+            return r;
+        }
+    }
+
+    //when length >=3
+    class Solution2 {
         public int peakIndexInMountainArray(int[] arr) {
             int n = arr.length;
             int l = 0;
