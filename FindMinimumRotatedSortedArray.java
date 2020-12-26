3c815e94ddbe802b94c1e9083220931eaa770e04
arjunmullick <arjunmullick@gmail.com>
2020-12-26 00:37:18 -0500

    Add files via upload

--- FindMinimumRotatedSortedArray.java
+++ FindMinimumRotatedSortedArray.java
@@ -2,19 +2,23 @@ package com.leetcode;
 
 public class FindMinimumRotatedSortedArray {
 
+    //4 , 5, 6 , 7 , 2, 3
+    // mid 6 > r 3 - means min is on right
+    // else its on left
     class Solution {
-
         public int findMin(int[] nums) {
-            int l = 0, r = nums.length-1;
-            while (l <= r) {
+            int n = nums.length;
+            int l = 0;
+            int r = n-1;
+            while(l<r){
                 int mid = l + (r-l)/2;
-                if (nums[mid] < nums[r]) {
+                if(nums[mid] < nums[r]){ // irrlevant nums[mid] < nums[mid+1] &&
                     r = mid;
-                } else {
+                }else{
                     l = mid+1;
                 }
             }
-            return nums[l-1];
+            return nums[r];
         }
     }
 }
