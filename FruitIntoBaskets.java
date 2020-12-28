eba6c7b94f74d086a45aff5c23201798f08e3066
arjunmullick <arjunmullick@gmail.com>
2020-12-28 00:55:33 -0500

    Add files via upload

---
+++ FruitIntoBaskets.java
@@ -0,0 +1,59 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FruitIntoBaskets {
+
+    class Solution {
+        // This is Longest Subarray With 2 Elements
+        //You have two baskets, and each basket can carry any quantity of fruit, but you want each basket to only carry one type of fruit each.
+        //starting at any type of tree find maz fruit can be collected
+
+
+        public int totalFruit(int[] tree) {
+            Map<Integer, Integer> count = new HashMap<>();
+            int max = 0;
+            int l = 0;
+            int r = 0;
+            int n = tree.length;
+            while(l < n && r < n) {
+                count.put(tree[r], count.getOrDefault(tree[r], 0) + 1);
+                while(count.size() > 2) {
+                    count.put(tree[l], count.get(tree[l]) - 1);
+                    count.remove(tree[l++], 0); //remove when match
+                }
+                r++;
+                max = Math.max(max,r-l);
+
+            }
+            return max;
+        }
+    }
+
+
+    //Alternatively we dont need a left pointer to traverse till all tree[l] are removed
+    //we can keep length counted as
+    class Solution2 {
+        // This is Longest Subarray With 2 Elements
+        //You have two baskets, and each basket can carry any quantity of fruit, but you want each basket to only carry one type of fruit each.
+        //starting at any type of tree find maz fruit can be collected
+
+
+        public int totalFruit(int[] tree) {
+            Map<Integer, Integer> count = new HashMap<>();
+            int l = 0;
+            int r = 0;
+            int n = tree.length;
+            while(l < n && r < n) {
+                count.put(tree[r], count.getOrDefault(tree[r], 0) + 1);
+                if(count.size() > 2) {//max is just difference of r and l
+                    count.put(tree[l], count.get(tree[l]) - 1);
+                    count.remove(tree[l++], 0); //remove when match
+                }
+                r++;
+            }
+            return r-l;
+        }
+    }
+}
