abd98991116fd363e8212e2bcdcf8e66931e29de
arjunmullick <arjunmullick@gmail.com>
2020-12-29 18:04:09 -0500

    Update PopulatingNextRightPointersEachNodeII.java

--- PopulatingNextRightPointersEachNodeII.java
+++ PopulatingNextRightPointersEachNodeII.java
@@ -1,4 +1,6 @@
-//https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii
+
+
+
 class Solution {
     public Node connect(Node root) {
         if(root == null) return null;
@@ -16,12 +18,29 @@ class Solution {
                 if(node.left != null) level.add(node.left);
                 if(node.right != null) level.add(node.right);
             }
-            //for(int i = 0 ; i < level.size() -1 ; i++){
-            //    Node n = level.get(i);
-            //    n.next = level.get(i+1);
-            //}
             queue = level;
         }
         return root;
+        
     }
 }
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
