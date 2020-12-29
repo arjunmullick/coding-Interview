d9f5795739b087d357caba024fa0f1e356dedf9a
arjunmullick <arjunmullick@gmail.com>
2020-12-28 23:07:43 -0500

    Add files via upload

---
+++ CourseSchedule.java
@@ -0,0 +1,56 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class CourseSchedule {
+
+    //https://leetcode.com/problems/course-schedule/
+
+    //O(∣E∣+∣V∣) no of edge + vertices
+    class Solution {
+
+        //[0,1]   0 needs 1 to complete 0<-1
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            HashMap<Integer,List<Integer>> adj = new HashMap<>();
+            int inDegree [] = new int[numCourses];
+
+            for(int i = 0 ; i < numCourses ; i++){
+                adj.put(i,new ArrayList<>());
+            }
+            for(int [] edge : prerequisites){
+
+                int node1 = edge[0];//take
+                int node2 = edge[1];//next
+
+                inDegree[node1] = inDegree[node1] + 1;
+                List<Integer> neighbours = adj.get(node2);
+                neighbours.add(node1);
+                adj.put(node2,neighbours);
+
+            }
+
+            Queue<Integer> queue = new LinkedList<>();
+            for(int i = 0 ; i < numCourses ; i++){
+                if(inDegree[i] == 0) queue.add(i);
+            }
+
+            while(queue.size() > 0){
+                int node = queue.poll();
+                for(int next : adj.get(node)){
+                    inDegree[next] = inDegree[next] - 1;
+                    if(inDegree[next] == 0){
+                        queue.offer(next);
+                    }
+                }
+            }
+            for(int i = 0 ; i < numCourses ; i++){
+                if(inDegree[i] != 0) return false;
+            }
+            return true;
+        }
+    }
+}
