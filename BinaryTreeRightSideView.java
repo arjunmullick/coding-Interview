2fb6d9168c4481c4d883cf5feb30b34fc36b6601
arjunmullick <arjunmullick@gmail.com>
2020-12-29 22:52:30 -0500

    Create BinaryTreeRightSideView.java

---
+++ BinaryTreeRightSideView.java
@@ -0,0 +1,49 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeRightSideView {
+
+    class Solution {
+        public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> result = new ArrayList<>();
+            if(root == null){
+                return result;
+            }
+
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+
+            while(queue.size() > 0){
+                int size = queue.size();
+                while(size > 0){
+                    TreeNode node = queue.poll();
+                    size--;
+                    if(size == 0)
+                        result.add(node.val);
+                    if(node.left != null) queue.offer(node.left);
+                    if(node.right != null) queue.offer(node.right);
+                }
+            }
+
+            return result;
+        }
+    }
+
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
