4ece1f85d76e04f06674cdcceba5d74f8e8897a6
arjunmullick <arjunmullick@gmail.com>
2021-01-07 00:07:01 -0500

    Create SubarrayProductLessThanK.java

---
+++ SubarrayProductLessThanK.java
@@ -0,0 +1,62 @@
+package com.leetcode;
+
+public class SubarrayProductLessThanK {
+
+    //https://leetcode.com/problems/subarray-product-less-than-k/
+
+    class Solution {
+
+        // 10 5 2 6
+        //--           10
+        //-----        10  5
+        //    -            5
+        //    ---          5 2
+        //    -----        5 2 6
+        public int numSubarrayProductLessThanK(int[] nums, int k) {
+            if (k <= 1) return 0;//see note below
+            int n = nums.length;
+            int l = 0;
+            int r = 0;
+            int prod = 1;
+
+            int result =0;
+            while(l<=r && r<n){
+                prod = prod * nums[r];//increase
+                while(prod >= k ){//can add l<=r to cover case k=0 but we checked edge condition above
+                    prod = prod / nums[l];//reduce product Note:Array of positive integers so no divide by zero
+                    l++;
+                }
+                result = result + (r-l)+1;//r=l=0 is one solution
+                r++;
+            }
+            return result;
+        }
+    }
+
+    //Recursive approach
+    class SolutionTLE {
+        int result;
+        public int numSubarrayProductLessThanK(int[] nums, int k) {
+            result = 0;
+            for(int i = 0 ; i < nums.length ; i++){
+                backtrack(nums,i,nums[i],k);
+            }
+
+            return result;
+        }
+
+        public void backtrack(int[] nums, int pos, int product, int k){
+            if(pos == nums.length) return;
+
+            if(product < k){
+                result++;
+            }else{
+                return;
+            }
+
+            if(pos < nums.length -1){
+                backtrack(nums,pos+1,product*nums[pos+1] , k);
+            }
+        }
+    }
+}
