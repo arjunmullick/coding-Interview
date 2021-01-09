2c501bc52e5bd15e43a81f3973be5d9c479b44a6
arjunmullick <arjunmullick@gmail.com>
2021-01-09 00:58:17 -0500

    Create RandomPickWeight.java

---
+++ RandomPickWeight.java
@@ -0,0 +1,37 @@
+package com.leetcode;
+
+import java.util.Random;
+
+public class RandomPickWeight {
+
+    //https://leetcode.com/problems/random-pick-with-weight/
+    class Solution {
+
+        Random random;
+        int[] cdf;
+        public Solution(int[] w) {
+            random = new Random();
+            cdf = new int[w.length];
+            cdf[0] = w[0];
+            for(int i = 1; i < w.length; i++) {
+                cdf[i] = cdf[i - 1] + w[i];
+            }
+        }
+
+        public int pickIndex() {
+            int n = cdf.length;
+            int target = random.nextInt(cdf[n - 1]) + 1;
+            int left = 0;
+            int right = n - 1;
+            while(left < right) {
+                int mid = (left + right) / 2;
+                if(cdf[mid] == target)
+                    return mid;
+                if(cdf[mid] < target)
+                    left = mid + 1;
+                else right = mid;
+            }
+            return left;
+        }
+    }
+}
