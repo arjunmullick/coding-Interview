1957e9dba63de55469725852f26bfad3e9709dbf
arjunmullick <arjunmullick@gmail.com>
2021-03-19 02:06:28 -0400

    Update CoinChange.java

--- CoinChange.java
+++ CoinChange.java
@@ -2,7 +2,29 @@ package com.leetcode;
 
 public class CoinChange {
     //https://leetcode.com/problems/coin-change/submissions/
-    class Solution {
+    //BottomUp
+    class SolutionBottomUp {
+        public int coinChange(int[] coins, int amount) {
+            int dp[] = new int[amount+1];
+            Arrays.fill(dp,amount+1); // Integer.MAX_VALUE
+            int n = coins.length;
+            dp[0] = 0;
+            for(int i = 1 ; i < amount+1 ; i++){
+                for(int j = 0 ; j < n; j++){
+                    int diff = i - coins[j];
+                    if(diff >=0 ){
+                        dp[i] = Math.min(dp[i] , dp[diff]+1);
+                    }
+                }
+            }
+
+            return dp[amount] > amount ? -1 : dp[amount];
+        }
+    }
+    
+    
+    //TopDown
+    class SolutionTopDown {
         public int coinChange(int[] coins, int amount) {
             int[] memo = new int[amount+1];
             return coinChangeRec(coins,amount,memo);
