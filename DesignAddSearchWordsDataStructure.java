f39a5dd60ed60961991e32ad8e985df085931d5d
arjunmullick <arjunmullick@gmail.com>
2021-04-10 18:46:13 -0400

    Create DesignAddSearchWordsDataStructure.java

---
+++ DesignAddSearchWordsDataStructure.java
@@ -0,0 +1,88 @@
+//https://leetcode.com/problems/design-add-and-search-words-data-structure/
+class WordDictionary {
+
+    /** Initialize your data structure here. */
+    Trie trie;
+    public WordDictionary() {
+        trie = new Trie();
+    }
+    
+    public void addWord(String word) {
+        trie.insert(word);
+    }
+    
+    public boolean search(String word) {
+        return trie.search(word);
+    }
+}
+
+class Trie{
+    TrieNode root;
+    
+    public Trie(){
+        root = new TrieNode();
+    }
+    
+    public void insert(String word){
+
+        TrieNode node = root;
+        for(char ch : word.toCharArray()){
+            TrieNode next = node.childs.getOrDefault(ch,new TrieNode());
+            node.childs.put(ch,next);
+            node = next;
+        }
+        node.isWord = true;
+        node.word = word;
+    }
+    
+    public boolean search(String word){
+        
+
+        Queue<TrieNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        int index = 0;
+        
+        while(queue.size() > 0){
+            Queue<TrieNode> level = new LinkedList<>();
+            while(queue.size() >0){
+                TrieNode node = queue.poll();
+                if(node.isWord && word.length() == index) return true;
+                if(index >= word.length()) continue;//ignor this word important not to return false as other branch may exist
+                char ch = word.charAt(index);
+                
+                HashMap<Character,TrieNode> childs = node.childs;
+                if(childs.containsKey(ch)){
+                    level.add(childs.get(ch));
+                }
+                if(ch == '.'){
+                    level.addAll(childs.values());
+                }
+            }
+            queue = level;
+            index++;//move to next alphabet in work
+        }
+        return false;
+    }
+    
+}
+
+
+class TrieNode{
+    
+    String word;
+    HashMap<Character,TrieNode> childs;
+    boolean isWord;
+    
+    public TrieNode(){
+        childs = new HashMap<>();
+        word = "";
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
