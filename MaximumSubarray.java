0bb8e16eb50a3e3a82359846c3ed86a5cea5a1a0
arjunmullick <arjunmullick@gmail.com>
2024-02-22 21:07:54 -0800

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -3,6 +3,23 @@ package com.leetcode;
 public class MaximumSubarray {
 
     //https://leetcode.com/problems/maximum-subarray/
+    /**
+        Example 1:
+        
+        Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+        Output: 6
+        Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+        Example 2:
+        
+        Input: nums = [1]
+        Output: 1
+        Explanation: The subarray [1] has the largest sum 1.
+        Example 3:
+        
+        Input: nums = [5,4,-1,7,8]
+        Output: 23
+        Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+    **/
     class Solution {
         public int maxSubArray(int[] nums) {
             int n = nums.length;
