c57125a709364f4b34c5cc62b1302d4093234590
arjunmullick <arjunmullick@gmail.com>
2024-02-28 22:36:25 -0800

    Update TaskScheduler.java

--- TaskScheduler.java
+++ TaskScheduler.java
@@ -6,6 +6,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
+/**
+    You are given an array of CPU tasks, each represented by letters A to Z, and a cooling time, n. 
+    Each cycle or interval allows the completion of one task. Tasks can be completed in any order, but there's a constraint: identical tasks must be separated by at least n intervals due to cooling time.
+    ​Return the minimum number of intervals required to complete all tasks.
+
+    Example 1:
+    
+    Input: tasks = ["A","A","A","B","B","B"], n = 2  
+    Output: 8
+    Explanation: A possible sequence is: A -> B -> idle -> A -> B -> idle -> A -> B.
+**/
+
 public class TaskScheduler {
     class Solution {
         //Complete the task type(A-Z) with max count first followed by second most
