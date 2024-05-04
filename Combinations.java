af2404dd409fcf05e2f97878e9773d83d75890b0
arjunmullick <arjunmullick@gmail.com>
2024-05-04 16:34:08 -0700

    Update Combinations.java

--- Combinations.java
+++ Combinations.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class Combinations {
     //https://leetcode.com/problems/combinations/
+    /**
+        Input: n = 4, k = 2
+        Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+        Explanation: There are 4 choose 2 = 6 total combinations.
+        Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+    **/
     class Solution {
         public List<List<Integer>> combine(int n, int k) {
             List<List<Integer>> result = new ArrayList<>();
