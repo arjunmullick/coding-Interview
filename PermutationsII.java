b14c1f8fde8c1a518e557ab87b53a29b5989e344
arjunmullick <arjunmullick@gmail.com>
2024-05-04 16:33:00 -0700

    Update PermutationsII.java

--- PermutationsII.java
+++ PermutationsII.java
@@ -4,6 +4,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+    Example 1:
+
+    Input: nums = [1,1,2]
+    Output:
+    [[1,1,2],
+     [1,2,1],
+     [2,1,1]]
+     
+    Example 2:
+    
+    Input: nums = [1,2,3]
+    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+**/
 public class PermutationsII {
 
     class Solution {
