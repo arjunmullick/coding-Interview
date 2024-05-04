4847540cf4394f6591a9a4c9b37d312b45bb50a7
arjunmullick <arjunmullick@gmail.com>
2024-05-04 16:30:59 -0700

    Update Permutations.java

--- Permutations.java
+++ Permutations.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Permutations {
     //https://leetcode.com/problems/permutations/
+    /**
+        Input: nums = [1,2,3]
+        Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+    **/
     class Solution {
         public List<List<Integer>> permute(int[] nums) {
 
