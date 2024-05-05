0f52f61591dca1af7e0788eb0162d4f740c8dca4
arjunmullick <arjunmullick@gmail.com>
2024-05-04 17:23:48 -0700

    Update TargetSum.java

--- TargetSum.java
+++ TargetSum.java
@@ -5,6 +5,17 @@ public class TargetSum {
     class Solution {
 
         //O(2^N) time and O(2^N) memory on stack
+        /**
+            Input: nums = [1,1,1,1,1], target = 3
+            
+            Output: 5
+            Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+            -1 + 1 + 1 + 1 + 1 = 3
+            +1 - 1 + 1 + 1 + 1 = 3
+            +1 + 1 - 1 + 1 + 1 = 3
+            +1 + 1 + 1 - 1 + 1 = 3
+            +1 + 1 + 1 + 1 - 1 = 3
+        **/
         int result;
         public int findTargetSumWays(int[] nums, int S) {
             result = 0;
