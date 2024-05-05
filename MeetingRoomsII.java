58d762bf95ed4e1eaa33c437f676d2adabcf7c39
arjunmullick <arjunmullick@gmail.com>
2024-05-05 10:04:36 -0700

    Update MeetingRoomsII.java

--- MeetingRoomsII.java
+++ MeetingRoomsII.java
@@ -3,6 +3,9 @@ package com.leetcode;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
+/**
+Given an array of meeting time intervals consisting of start and end times[[s1,e1],[s2,e2],...](si< ei), find the minimum number of conference rooms required.
+**/
 public class MeetingRoomsII {
 
     class Solution {
