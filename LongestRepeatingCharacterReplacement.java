6eb742d274293190fd0c4b67ea25f108f0b80361
arjunmullick <arjunmullick@gmail.com>
2024-05-05 11:07:42 -0700

    Update LongestRepeatingCharacterReplacement.java

--- LongestRepeatingCharacterReplacement.java
+++ LongestRepeatingCharacterReplacement.java
@@ -3,6 +3,21 @@ package com.leetcode;
 public class LongestRepeatingCharacterReplacement {
 
     //https://leetcode.com/problems/longest-repeating-character-replacement/
+    /**
+    Example 1:
+
+    Input: s = "ABAB", k = 2
+    Output: 4
+    Explanation: Replace the two 'A's with two 'B's or vice versa.
+    Example 2:
+    
+    Input: s = "AABABBA", k = 1
+    Output: 4
+    Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+    The substring "BBBB" has the longest repeating letters, which is 4.
+    There may exists other ways to achieve this answer too.
+
+    **/
     class Solution {
         public int characterReplacement(String s, int k) {
             int left = 0;
