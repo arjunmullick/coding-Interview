93139c43dd7b36a14519e36a067367c09649ffc9
arjunmullick <arjunmullick@gmail.com>
2024-05-05 10:30:03 -0700

    Update FindKClosestElements.java

--- FindKClosestElements.java
+++ FindKClosestElements.java
@@ -8,6 +8,25 @@ import java.util.stream.Collectors;
 public class FindKClosestElements {
 
     /*
+
+Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+An integer a is closer to x than an integer b if:
+
+|a - x| < |b - x|, or
+|a - x| == |b - x| and a < b
+ 
+
+Example 1:
+
+Input: arr = [1,2,3,4,5], k = 4, x = 3
+Output: [1,2,3,4]
+Example 2:
+
+Input: arr = [1,2,3,4,5], k = 4, x = -1
+Output: [1,2,3,4]
+
+    
        By Closest value
         [1,2,3,4,10,12]
         k = 4
