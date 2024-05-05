68e47de87122be0419a2bed8e188e7a5dc124913
arjunmullick <arjunmullick@gmail.com>
2024-05-04 18:25:32 -0700

    Update MaximumProductSubarray.java

--- MaximumProductSubarray.java
+++ MaximumProductSubarray.java
@@ -3,6 +3,18 @@ package com.leetcode;
 public class MaximumProductSubarray {
 
     //https://leetcode.com/problems/maximum-product-subarray/
+    /**
+    Example 1:
+
+        Input: nums = [2,3,-2,4]
+        Output: 6
+        Explanation: [2,3] has the largest product 6.
+    Example 2:
+        
+        Input: nums = [-2,0,-1]
+        Output: 0
+        Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+    **/
     class Solution {
 
     /*
