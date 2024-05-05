1aceedf6bd1b0c866966a3ec034a1a67cdc78c9c
arjunmullick <arjunmullick@gmail.com>
2024-05-04 18:21:29 -0700

    Update HouseRobberII.java

--- HouseRobberII.java
+++ HouseRobberII.java
@@ -4,6 +4,19 @@ import java.util.Arrays;
 
 public class HouseRobberII {
     //https://leetcode.com/problems/house-robber-ii/
+    /**
+        Example 1:
+        
+        Input: nums = [2,3,2]
+        Output: 3
+        Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+        Example 2:
+        
+        Input: nums = [1,2,3,1]
+        Output: 4
+        Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+        Total amount you can rob = 1 + 3 = 4.
+    **/
     class Solution {
         public int rob(int[] nums) {
             int n = nums.length;
