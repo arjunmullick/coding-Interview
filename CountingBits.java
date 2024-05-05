f4a71452fe3666420dd7d44e270b439b3522a6f4
arjunmullick <arjunmullick@gmail.com>
2024-05-04 19:02:09 -0700

    Update CountingBits.java

--- CountingBits.java
+++ CountingBits.java
@@ -4,6 +4,29 @@ public class CountingBits {
 
     //https://leetcode.com/problems/counting-bits/
 
+    /**
+    Example 1:
+    
+    Input: n = 2
+    Output: [0,1,1]
+    Explanation:
+    0 --> 0
+    1 --> 1
+    2 --> 10
+    Example 2:
+    
+    Input: n = 5
+    Output: [0,1,1,2,1,2]
+    Explanation:
+    0 --> 0
+    1 --> 1
+    2 --> 10
+    3 --> 11
+    4 --> 100
+    5 --> 101
+ 
+    **/
+
     //O(N*K)
     class Solution {
         public int[] countBits(int num) {
