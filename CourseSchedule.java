d710b7c09d8f93535a90cd87bdfe8dc5cb4a5883
arjunmullick <arjunmullick@gmail.com>
2024-05-05 11:24:37 -0700

    Update CourseSchedule.java

--- CourseSchedule.java
+++ CourseSchedule.java
@@ -15,6 +15,13 @@ public class CourseSchedule {
     Explanation: There are a total of 2 courses to take. 
     To take course 1 you should have finished course 0. So it is possible.
 
+    Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+    Output: false
+    Explanation: There are a total of 2 courses to take. 
+    To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. 
+    So it is impossible.
+
+
     **/
 
     //O(∣E∣+∣V∣) no of edge + vertices
