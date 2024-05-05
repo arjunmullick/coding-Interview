63b976a168fa1ea90aaf936412e7503bd3077423
arjunmullick <arjunmullick@gmail.com>
2024-05-04 17:00:54 -0700

    Update CombinationSumIII.java

--- CombinationSumIII.java
+++ CombinationSumIII.java
@@ -6,6 +6,33 @@ import java.util.List;
 public class CombinationSumIII {
 
     //https://leetcode.com/problems/combination-sum-iii/
+    /**
+
+    Example 1:
+
+        Input: k = 3, n = 7
+        Output: [[1,2,4]]
+        Explanation:
+        1 + 2 + 4 = 7
+        There are no other valid combinations.
+        
+    Example 2:
+        
+        Input: k = 3, n = 9
+        Output: [[1,2,6],[1,3,5],[2,3,4]]
+        Explanation:
+        1 + 2 + 6 = 9
+        1 + 3 + 5 = 9
+        2 + 3 + 4 = 9
+        There are no other valid combinations.
+        
+    Example 3:
+        
+        Input: k = 4, n = 1
+        Output: []
+        Explanation: There are no valid combinations.
+        Using 4 different numbers in the range [1,9], the smallest sum we can get is 1+2+3+4 = 10 and since 10 > 1, there are no valid combination.
+    **/
     class Solution {
         public List<List<Integer>> combinationSum3(int k, int n) {
             List<Integer> nums = new ArrayList<>();
