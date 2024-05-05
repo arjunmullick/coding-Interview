1882b83d23c132d1bb7c753ecc94836d8401ba8e
arjunmullick <arjunmullick@gmail.com>
2024-05-04 18:40:00 -0700

    Update DecodeWays.java

--- DecodeWays.java
+++ DecodeWays.java
@@ -2,6 +2,23 @@ package com.leetcode;
 
 public class DecodeWays {
     //https://leetcode.com/problems/decode-ways/
+    /**
+    Example 1:
+
+    Input: s = "12"
+    Output: 2
+    Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+    Example 2:
+    
+    Input: s = "226"
+    Output: 3
+    Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+    Example 3:
+    
+    Input: s = "06"
+    Output: 0
+    Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
+    **/
     public class Solution {
         public int numDecodings(String s) {
             if(s == null || s.length() == 0) {
