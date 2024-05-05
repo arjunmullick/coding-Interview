70c472ca455ed4751743129a54297c339243b905
arjunmullick <arjunmullick@gmail.com>
2024-05-04 18:37:11 -0700

    Update WordBreak.java

--- WordBreak.java
+++ WordBreak.java
@@ -6,6 +6,22 @@ import java.util.List;
 
 public class WordBreak {
     //https://leetcode.com/problems/word-break/
+    /**
+    Example 1:
+    Input: s = "leetcode", wordDict = ["leet","code"]
+    Output: true
+    Explanation: Return true because "leetcode" can be segmented as "leet code".
+    
+    Example 2:
+    Input: s = "applepenapple", wordDict = ["apple","pen"]
+    Output: true
+    Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+    Note that you are allowed to reuse a dictionary word.
+    
+    Example 3:
+    Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+    Output: false
+    **/
     class Solution {
         public boolean wordBreak(String s, List<String> wordDict) {
             HashSet<String> set = new HashSet<>();
