4960dfef71c63d038e11598fabe437c772a5ceb6
arjunmullick <arjunmullick@gmail.com>
2024-05-05 10:17:03 -0700

    Update MinimumNumberArrowsBurstBalloons.java

--- MinimumNumberArrowsBurstBalloons.java
+++ MinimumNumberArrowsBurstBalloons.java
@@ -4,9 +4,7 @@ import java.util.Arrays;
 
 public class MinimumNumberArrowsBurstBalloons {
 
-    class Solution {
-
-        /*
+ /*
 
     Sorting by start need backtracking as we may miss
        0 9,0 6,2 9,2 8,3 9,3 8,3 9,6 8,7 12,9 10
@@ -37,6 +35,43 @@ public class MinimumNumberArrowsBurstBalloons {
                                  9--10
                             7------------12
         */
+
+    class Solution {
+    public int findMinArrowShots(int[][] points) {
+        
+
+        if(points == null || points.length == 0) return 0;
+        Arrays.sort(points,(a, b)->(Integer.compare (a[1] , b[1])));// .compare because testcase has int maxValue
+
+        //Arrays.sort(points,(a,b)->(a[1]-b[1]));
+        int count = 1;
+        int[] prev = points[0];
+        for(int i = 1 ; i < points.length ; i++){
+            int[] next = points[i];
+            if(isOverlap(prev,next)){
+                prev[0] = Math.max(prev[0],next[0]);
+                prev[1] = Math.min(prev[1],next[1]);
+            }else{
+                count++;
+                prev = next;
+            }
+ 
+        } 
+        return count;
+
+    }
+
+    public boolean isOverlap(int[] a , int[] b){
+        if((a[0] > b[1]) || (b[0] > a[1])) return false; // one is starting after other ends then no overlap
+        return true;
+    }
+}
+
+    
+
+    class Solution2 {
+
+       
         public int findMinArrowShots(int[][] points) {
 
             if(points.length <= 1) return points.length;
