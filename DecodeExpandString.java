a95d94cf69f6375009ddef175a0acde5ac6f5dbb
arjunmullick <arjunmullick@gmail.com>
2024-06-19 00:39:33 -0700

    Update DecodeExpandString.java

--- DecodeExpandString.java
+++ DecodeExpandString.java
@@ -1,3 +1,20 @@
+/*
+Example 1:
+
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+Example 2:
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+Example 3:
+
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+
+
+*/
+
 class Solution {
     public String decodeString(String s) {
         Deque<Integer> numStack = new ArrayDeque<>();
